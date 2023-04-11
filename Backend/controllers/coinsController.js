3b6f1959a56afbc4492ae59d3a750ed881f7a0ec
ArxLancifer <a_christoforidis@hotmail.com>
2023-04-11 22:08:13 +0300

    .

--- Backend/controllers/coinsController.js
+++ Backend/controllers/coinsController.js
@@ -6,7 +6,6 @@ const coinList = {
     getListOfCoins: async function (req, res) {
         try {
             const pagination = req.body.pagination;
-
             const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${pagination.pageSize}&page=${pagination.pageIndex}&sparkline=false&locale=en`)
 
             const data = response.data;
